9ad5e0e07a5db354324e9237dbf84a8fc51c032f
siyaram <sonu.sgsits08@gmail.com>
2019-12-15 09:54:28 +0530

    wrapper objects declaration.

---
+++ src/main/java/com/datatypes/Datatypes.java
@@ -0,0 +1,14 @@
+package com.datatypes;
+
+public class Datatypes 
+{
+public static void main(String[] args) {
+	//which of the following is valid wrapper object declaration ?
+	Long l1 = new Long("12345678");
+	Byte l2 = new Byte("01");
+	Character l3 = new Character("1");
+	Integer l4 = new Integer("12345678");
+	Boolean l5= new Boolean("12");
+	System.out.println(l1+" "+l2+" "+l4+" "+l5);
+}
+}
